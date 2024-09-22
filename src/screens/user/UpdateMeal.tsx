675d8d1b8cd6fa6bd65b4b0d94eca7c66e7c7f0b
Mohammad Fiaz Rahman Nafee <faiazrahman70@gmail.com>
2024-09-22 16:30:11 +0600

    Code refactoring for Calendar

--- src/screens/user/UpdateMeal.tsx
+++ src/screens/user/UpdateMeal.tsx
@@ -1,4 +1,4 @@
-import {View, Text, Pressable, ToastAndroid} from 'react-native';
+import {View, Text, Pressable, ToastAndroid, StyleSheet} from 'react-native';
 import React, {useCallback, useState} from 'react';
 
 import MainLayout from '@layouts/MainLayout';
@@ -17,79 +17,109 @@ export default function UpdateMeal() {
 
   const [objOfDates, setObjOfDates] = useState({});
 
-  React.useEffect(() => {
-    console.log(objOfDates, selected, selectedSecond);
-  }, [objOfDates, selected, selectedSecond]);
+  // React.useEffect(() => {
+  //   console.log(objOfDates, selected, selectedSecond);
+  // }, [objOfDates, selected, selectedSecond]);
 
-  const updateDateObj = useCallback(() => {
-    let obj: any = {};
-    if (selected && !selectedSecond) {
-      let timestamp = moment(selected).format('YYYY-MM-DD');
-      Object.assign(obj, {
+  const generateDateObj = (
+    year: string,
+    month: string,
+    sDay: number,
+    eDay: number,
+  ) => {
+    const dateObj: any = {};
+
+    for (let i = sDay; i <= eDay; ++i) {
+      dateObj[`${year}-${month}-${i}`] = {
         selected: true,
-        backgroundColor: '#fff',
-        selectedDotColor: 'red',
-      });
-
-      setObjOfDates(obj);
-    } else if (selected && selectedSecond) {
-      let startingDate: number = Number.parseInt(moment(selected).format('DD'));
-      let endingDate: number = Number.parseInt(
-        moment(selectedSecond).format('DD'),
-      );
-
-      let startingMonth: number = Number.parseInt(
-        moment(selected).format('MM'),
-      );
-
-      let startingYear: number = Number.parseInt(
-        moment(selected).format('YYYY'),
-      );
-
-      let currentDate = new Date(selected);
-      let endDate = new Date(selectedSecond);
-      while (currentDate <= endDate) {
-        let currentFormattedDate = moment(currentDate).format('YYYY-MM-DD');
-        obj[currentFormattedDate] = {
-          selected: true,
-          backgroundColor: 'red',
-          selectedDotColor: 'red',
-        };
-
-        currentDate.setDate(currentDate.getDate() + 1);
-      }
+        selectedColor: Colors.socialBlue,
+        marked: true,
+      };
     }
-  }, [selected, selectedSecond]);
+    return dateObj;
+  };
 
-  const handleDayPress = (day: DateData) => {
-    // console.log('selected day =>', day)
-    if (!selected) {
-      setSelected(day.dateString);
-    } else {
-      if (day.day <= parseInt(moment(selected).format('DD'))) {
-        if (day.day < new Date().getDate()) {
+  const handleDayPress = useCallback(
+    (day: DateData) => {
+      let obj: any = {};
+      if (!selected) {
+        // console.log(1);
+        if (day.day >= parseInt(moment().format('DD'))) {
+          setSelected(day.dateString);
+          // console.log(2);
+
+          obj[`${day.dateString}`] = {
+            selected: true,
+            selectedColor: Colors.socialBlue,
+            selectedDotColor: 'red',
+          };
+          setObjOfDates(obj);
+        } else {
+          // console.log(3);
+
           ToastAndroid.showWithGravity(
             'Cannot select a day from past!',
             1500,
             2,
           );
-        } else {
-          if (day.day === parseInt(moment(selected).format('DD'))) {
-            setSelectedSecond(day.dateString);
-          }
-          setSelected(day.dateString);
         }
       } else {
-        setSelectedSecond(day.dateString);
+        // console.log(4);
+
+        if (day.day <= parseInt(moment(selected).format('DD'))) {
+          // console.log(5);
+          if (day.day < parseInt(moment().format('DD'))) {
+            // console.log(6);
+
+            ToastAndroid.showWithGravity(
+              'Cannot select a day from past!',
+              1500,
+              2,
+            );
+          } else {
+            // console.log(7);
+            let first = selected;
+
+            setSelectedSecond(first);
+            setSelected(day.dateString);
+
+            const year = moment(first).format('YYYY');
+            const month = moment(first).format('MM');
+
+            const sDay = parseInt(moment(day.dateString).format('DD'));
+            const eDay = parseInt(moment(first).format('DD'));
+
+            const returnObj = generateDateObj(year, month, sDay, eDay);
+
+            setObjOfDates(returnObj);
+          }
+        } else {
+          // console.log(8);
+          setSelectedSecond(day.dateString);
+
+          const year = moment(selected).format('YYYY');
+          const month = moment(selected).format('MM');
+          const sDay = parseInt(moment(selected).format('DD'));
+          const eDay = parseInt(moment(day.dateString).format('DD'));
+
+          const returnObj = generateDateObj(year, month, sDay, eDay);
+
+          setObjOfDates(returnObj);
+        }
       }
-    }
-    updateDateObj();
-  };
+    },
+    [selected, selectedSecond, objOfDates],
+  );
 
   const formatDate = (): string => {
     if (selected && !selectedSecond) {
       return moment(selected).format('LL');
     } else if (selected && selectedSecond) {
+      if (selected === selectedSecond) {
+        let selectedFDate = moment(selected).format('LL');
+
+        return selectedFDate;
+      }
       let selectedFDate = moment(selected).format('LL');
       let selectedFSecondDate = moment(selectedSecond).format('LL');
 
@@ -100,7 +130,7 @@ export default function UpdateMeal() {
   };
 
   return (
-    <MainLayout noScroll={true}>
+    <MainLayout noScroll={false}>
       <View
         style={{
           height: 50,
@@ -124,11 +154,7 @@ export default function UpdateMeal() {
       </View>
 
       <Calendar
-        style={{
-          width: Dim.width * 0.9,
-          alignSelf: 'center',
-          borderRadius: 5,
-        }}
+        style={styles.calendar}
         theme={{
           calendarBackground: Colors.socialBlack,
           selectedDayTextColor: '#fff',
@@ -136,11 +162,10 @@ export default function UpdateMeal() {
           todayTextColor: Colors.socialBlue,
           dayTextColor: '#fff',
           arrowColor: Colors.socialPink,
-          selectedDayBackgroundColor: Colors.socialBlue,
         }}
         hideExtraDays={true}
         hideArrows={true}
-        markingType="period"
+        markingType="custom"
         onDayPress={handleDayPress}
         markedDates={objOfDates}
       />
@@ -157,9 +182,25 @@ export default function UpdateMeal() {
             justifyContent: 'center',
             marginTop: 25,
           }}>
-          <AppText styles={{color: Colors.socialWhite}}>{formatDate()}</AppText>
+          <AppText
+            styles={{
+              color: Colors.socialWhite,
+              fontSize: 14,
+              fontFamily: 'Roboto-Regular',
+            }}>
+            {formatDate()}
+          </AppText>
         </View>
       )}
     </MainLayout>
   );
 }
+
+const styles = StyleSheet.create({
+  calendar: {
+    width: Dim.width * 0.9,
+    alignSelf: 'center',
+    height: Dim.height * 0.55,
+    borderRadius: 5,
+  },
+});
